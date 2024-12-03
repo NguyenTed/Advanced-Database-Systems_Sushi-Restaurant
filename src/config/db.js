c5e175b9c88de65af1fe216aaeed5ee9c813d54c
Nguyễn Quốc Thuần <nguyenquocthuan2905@gmail.com>
2024-12-03 14:51:28 +0700

    connect to db using knex

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,14 @@
-import mysql from "mysql2";
+import knex from "knex";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = mysql
-  .createPool({
+export const db = knex({
+  client: "mysql2",
+  connection: {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-  })
-  .promise();
+  },
+  pool: { min: 2, max: 10 },
+});
