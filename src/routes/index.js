ed2f3a2ec4c73bc9146e95dec934b21e887ddee6
Tai Trinh <tatai22@clc.fitus.edu.vn>
2024-12-12 23:57:18 +0700

    làm landing page, tạo main layout cho website

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
 import { PATH } from "../config/path.js";
 
 const routes = Router();
 
-routes.get(PATH.HOME, async (req, res) => {
-  res.status(200).send("Hello world");
+routes.get("/", (req, res) => {
+  res.render("layout/main-layout", {
+    title: "Home",
+    description: "Home page of Online News",
+    content: "../pages/home.ejs",
+  });
+});
+
+routes.use((req, res) => {
+  res.status(404).render("layout/main-layout.ejs", {
+    title: "404 - Không tìm thấy",
+    description: "Nội dung bạn đang cố truy cập không tồn tại hoặc đã bị xóa",
+    content: "../pages/404",
+  });
 });
 
 export default routes;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 export default {
-  content: [
-    "./src/*.{html,js,css}",
-    "./src/views/**/*.ejs",
-    "./public/js/**/*.js",
-  ],
-  mode: "jit",
-  purge: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
+  content: ["./src/views/**/*.ejs"],
+  // mode: "jit",
+  // purge: ["./src/views/**/*.ejs"],
   theme: {
     extend: {},
   },
