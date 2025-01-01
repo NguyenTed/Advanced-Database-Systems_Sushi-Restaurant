c5afd1d6ce0293d12667d91782045cf2119700ae
Tai Trinh <tatai22@clc.fitus.edu.vn>
2025-01-01 10:31:58 +0700

    làm trang profile

--- src/routes/profile-route.js
+++ src/routes/profile-route.js
@@ -1,19 +1,45 @@
 import { Router } from 'express';
+import { db } from '../config/db.js';
+import bcrypt from 'bcrypt';
 
 const routes = Router();
 
-routes.get('/thong-tin-ca-nhan', (req, res) => {
-  let content = '';
+routes.get('/thong-tin-ca-nhan', async (req, res) => {
+  let membershipInfo = null;
   if (req.user.role === 'Khách hàng') {
-    content = '../pages/profile/customer.ejs';
-  } else {
-    content = '../pages/profile/employee.ejs';
+    membershipInfo = await db('membership_card').where('customer_id', req.profile.customer_id).first();
   }
+
   res.render('layout/main-layout', {
-    title: 'Samurai Sushi - Nhà hàng Nhật Bản',
-    description: 'Nhà hàng Nhật Bản cao cấp tại Sài Gòn',
-    content
+    title: 'Thông tin cá nhân | Samurai Sushi',
+    description: 'Thông tin cá nhân người dùng',
+    content: '../pages/profile.ejs',
+    user: req.user,
+    membershipInfo
   });
+});
+
+routes.post('/thong-tin-ca-nhan/doi-mat-khau', async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const userId = req.user.account_id;
+  console.log(userId);
+
+  try {
+    const user = await db('account').where('account_id', userId).first();
+    const isValidPassword = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isValidPassword) {
+      return res.status(400).send('Mật khẩu cũ không đúng');
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await db('account').where('account_id', userId).update({ password: hashedPassword });
+
+    res.status(200).send('Success');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Có lỗi xảy ra khi đổi mật khẩu');
+  }
 });
 
 export default routes;
